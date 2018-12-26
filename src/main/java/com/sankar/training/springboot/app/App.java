f9b395560244eb74eb2a0399fb80fc7016c84671
sanki-github <46144932+sanki-github@users.noreply.github.com>
2018-12-25 21:02:07 -0600

    Add files via upload

---
+++ src/main/java/com/sankar/training/springboot/app/App.java
@@ -0,0 +1,18 @@
+package com.sankar.training.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is my first Spring Boot Application" );
+        SpringApplication.run(App.class, args);
+    }
+}
